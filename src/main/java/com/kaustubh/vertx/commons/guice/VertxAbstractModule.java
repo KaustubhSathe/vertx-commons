7147a93941ac24b8a352141faff365a08bf1ea58
kaustubhsathefc <89383411+kaustubhsathefc@users.noreply.github.com>
2021-12-30 15:06:07 +0530

    change

--- src/main/java/com/kaustubh/vertx/commons/guice/VertxAbstractModule.java
+++ src/main/java/com/kaustubh/vertx/commons/guice/VertxAbstractModule.java
@@ -1,11 +1,9 @@
 package com.kaustubh.vertx.commons.guice;
 
 import com.google.inject.AbstractModule;
-import io.vertx.core.Vertx;
+
 
 public abstract class VertxAbstractModule extends AbstractModule {
-    public VertxAbstractModule(Vertx vertx){}
-
     @Override
     protected void configure(){
         bindConfiguration();
