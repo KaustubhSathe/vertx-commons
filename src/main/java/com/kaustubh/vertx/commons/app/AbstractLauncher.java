a7cb22739e1460adbd91552b625197d58e13168b
KaustubhSathe <f2015466p@alumni.bits-pilani.ac.in>
2021-12-31 16:09:15 +0530

    change

--- src/main/java/com/kaustubh/vertx/commons/app/AbstractLauncher.java
+++ src/main/java/com/kaustubh/vertx/commons/app/AbstractLauncher.java
@@ -1,8 +1,12 @@
 package com.kaustubh.vertx.commons.app;
 
 import com.google.inject.Module;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Launcher;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.impl.cpu.CpuCoreSensor;
+import io.vertx.core.json.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -11,10 +15,30 @@ import java.util.List;
 public abstract class AbstractLauncher extends Launcher {
     public static final Integer NUM_OF_CORES = CpuCoreSensor.availableProcessors();
 
-    protected abstract Deployable[] getVerticlesToDeploy();
-
     protected abstract Module[] getGoogleGuiceModules();
 
-//    private List<Module> getALlGoogleGuiceModules
+    public AbstractLauncher() {
+        super();
+    }
 
+    @Override
+    public abstract void afterConfigParsed(JsonObject config);
+
+    @Override
+    public abstract void beforeStartingVertx(VertxOptions options);
+
+    @Override
+    public abstract void afterStartingVertx(Vertx vertx);
+
+    @Override
+    public abstract void beforeDeployingVerticle(DeploymentOptions deploymentOptions);
+
+    @Override
+    public abstract void beforeStoppingVertx(Vertx vertx);
+
+    @Override
+    public abstract void afterStoppingVertx();
+
+    @Override
+    public abstract void handleDeployFailed(Vertx vertx, String mainVerticle, DeploymentOptions deploymentOptions, Throwable cause);
 }
