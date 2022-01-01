cb641933adf40976ab173fb4f351866bad939809
KaustubhSathe <f2015466p@alumni.bits-pilani.ac.in>
2022-01-02 03:51:50 +0530

    change

--- src/main/java/com/kaustubh/vertx/commons/guice/VertxAbstractModule.java
+++ src/main/java/com/kaustubh/vertx/commons/guice/VertxAbstractModule.java
@@ -2,12 +2,11 @@ package com.kaustubh.vertx.commons.guice;
 
 import com.google.inject.AbstractModule;
 
-
 public abstract class VertxAbstractModule extends AbstractModule {
-    @Override
-    protected void configure(){
-        bindConfiguration();
-    }
+  @Override
+  protected void configure() {
+    bindConfiguration();
+  }
 
-    protected abstract void bindConfiguration();
+  protected abstract void bindConfiguration();
 }
